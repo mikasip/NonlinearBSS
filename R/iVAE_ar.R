25d1e89fb351c02a4f7f8e89ef3520741fb49d2f
mikasip <mikasip11@gmail.com>
2025-03-27 11:40:00 +0200

    cleaning

--- R/iVAE_ar.R
+++ R/iVAE_ar.R
@@ -1,13 +1,12 @@
-#' Identifiable Variational Autoencoder with AR(1) Latent Structure
+#' Identifiable Variational Autoencoder with AR(R) Latent Structure
 #'
-#' This function fits an Identifiable Variational Autoencoder (iVAE) model where the latent variables follow an AR(1) process. It also supports handling auxiliary data and accommodates various choices for source and error distributions.
+#' This function fits an Identifiable Variational Autoencoder (iVAE) model where the latent variables follow an AR(R) process, R being the autoregressive order. It also supports handling auxiliary data and accommodates various choices for source and error distributions.
 #'
 #' @param data A numeric matrix of observed data (n x p), where n is the number of samples, and p is the number of features.
 #' @param aux_data A numeric matrix of auxiliary data (n x q), where q is the number of auxiliary variables.
 #' @param latent_dim An integer specifying the number of latent dimensions.
-#' @param data_prev Optional. A numeric matrix of previously observed data, used for constructing the AR(1) latent prior. If not provided, the function constructs it automatically.
-#' @param test_data Optional. A numeric matrix of test data used for validation.
-#' @param test_data_aux Optional. A numeric matrix of auxiliary data corresponding to the test data.
+#' @param prev_data_list A list of numeric matrices of previously observed data, used for constructing the AR(R) latent prior.
+#' @param prev_aux_data_list A list of numeric matrices of auxiliary data for the previous observations.
 #' @param hidden_units A numeric vector specifying the number of units in each hidden layer for the encoder and decoder.
 #' @param aux_hidden_units A numeric vector specifying the number of units in each hidden layer for modeling the prior distribution using the auxiliary data.
 #' @param activation A character string specifying the activation function to be used in hidden layers. Default is `"leaky_relu"`.
@@ -18,10 +17,9 @@
 #' @param optimizer An optional Keras optimizer object. If NULL (default), an Adam optimizer with polynomial decay is used.
 #' @param lr_start A numeric value for the initial learning rate. Default is 0.001.
 #' @param lr_end A numeric value for the final learning rate. Default is 0.0001.
-#' @param steps An integer specifying the number of optimization steps for learning rate decay. Default is 10,000.
+#' @param steps An integer specifying the number of optimization steps for polynomial learning rate decay. Default is 10,000.
 #' @param seed An optional integer for setting the random seed to ensure reproducibility. Default is NULL.
-#' @param get_prior_means Logical. If TRUE, the model returns the prior means and variances. Default is TRUE.
-#' @param true_data Optional. A numeric matrix of ground truth data for calculating the mean correlation coefficient (MCC) during training.
+#' @param get_elbo Logical. If TRUE, the model returns the final ELBO value. Default is FALSE.
 #' @param epochs An integer specifying the number of training epochs.
 #' @param batch_size An integer specifying the batch size for training.
 #'
@@ -54,11 +52,10 @@
 #'   print(model)
 #' }
 #' @export
-iVAEar <- function(data, aux_data, latent_dim, prev_data_list, prev_aux_data_list, test_data = NULL, test_data_aux = NULL, hidden_units = c(128, 128, 128), aux_hidden_units = c(128, 128, 128),
+iVAEar <- function(data, aux_data, latent_dim, prev_data_list, prev_aux_data_list, hidden_units = c(128, 128, 128), aux_hidden_units = c(128, 128, 128),
                      activation = "leaky_relu", source_dist = "gaussian", validation_split = 0, error_dist = "gaussian",
                      error_dist_sigma = 0.01, optimizer = NULL, lr_start = 0.001, lr_end = 0.0001, ar_order = 1,
-                     steps = 10000, seed = NULL, get_prior_means = TRUE, epoch_thresholds = NULL, 
-                     threshold_batch_sizes = NULL, epochs, initial_batch_size) {
+                     steps = 10000, seed = NULL, get_elbo = TRUE, epochs, batch_size) {
   source_dist <- match.arg(source_dist, c("gaussian", "laplace"))
   source_log_pdf <- switch(source_dist,
     "gaussian" = norm_log_pdf,
@@ -95,14 +92,6 @@ iVAEar <- function(data, aux_data, latent_dim, prev_data_list, prev_aux_data_lis
     prev_data_list[[i]] <- sweep(prev_data_list[[i]], 2, data_sds, "/")
   }
 
-  test_data_scaled <- NULL
-  if (!is.null(test_data)) {
-    test_data_cent <- sweep(test_data, 2, data_means, "-")
-    test_data_scaled <- sweep(test_data_cent, 2, data_sds, "/")
-    test_data_prev <- rbind(rep(0, p), test_data_scaled[1:(n - 1), ])
-  }
-
-  
   if (!is.null(seed)) {
     tf$keras$utils$set_random_seed(as.integer(seed))
   }
@@ -161,9 +150,7 @@ iVAEar <- function(data, aux_data, latent_dim, prev_data_list, prev_aux_data_lis
   z_log_var_layer <- layer_dense(units = latent_dim)
   z_mean <- submodel %>% z_mean_layer()
   z_log_var <- submodel %>% z_log_var_layer()
-  #z_prev_log_var <- submodel_prev %>% z_log_var_layer
   z_mean_and_var <- layer_concatenate(list(z_mean, z_log_var))
-  #z_prev_mean_and_var <- layer_concatenate(list(z_prev_mean, z_prev_log_var))
   encoder <- keras_model(list(input_data, aux_input), z_mean)
   z_log_var_model <- keras_model(list(input_data, aux_input), z_log_var)
 
@@ -176,25 +163,20 @@ iVAEar <- function(data, aux_data, latent_dim, prev_data_list, prev_aux_data_lis
     "laplace" = sampling_laplace(p = latent_dim)
   )
   z <- z_mean_and_var %>% sampling_layer()
-  #z_prev <- z_prev_mean_and_var %>% sampling_layer()
 
   x_decoded_mean <- z
-  #x_prev_decoded_mean <- z_prev
   input_decoder <- layer_input(latent_dim)
   output_decoder <- input_decoder
   for (n_units in rev(hidden_units)) {
     dense_layer <- layer_dense(units = n_units, activation = activation)
     x_decoded_mean <- x_decoded_mean %>%
       dense_layer()
-    #x_prev_decoded_mean <- x_prev_decoded_mean %>% dense_layer()
     output_decoder <- output_decoder %>% dense_layer()
   }
   out_layer <- layer_dense(units = p)
   x_decoded_mean <- x_decoded_mean %>% out_layer()
-  #x_prev_decoded_mean <- x_prev_decoded_mean %>% out_layer()
   output_decoder <- output_decoder %>% out_layer()
   decoder <- keras_model(input_decoder, output_decoder)
-  #final_output <- layer_concatenate(list(x_decoded_mean, z, z_mean_and_var, z_prev, z_prev_mean_and_var, prior_v, x_prev_decoded_mean, input_prev_obs))
   output <- list(x_decoded_mean, z, z_mean_and_var, prior_v, mask_input)
   output <- append(output, prev_z)
   output <- append(output, prev_prior_means)
@@ -224,14 +206,11 @@ iVAEar <- function(data, aux_data, latent_dim, prev_data_list, prev_aux_data_lis
         prior_mean_final <- prior_mean_final + ar_prev_mult[, ((i - 1) * latent_dim + 1):(i * latent_dim)]
       }
     }
-    # Calculate each component separately for debugging
     log_px_z_unreduced <- error_log_pdf(x, x_mean, tf$constant(error_dist_sigma, "float32"), reduce = FALSE)
       
-    # Apply mask
     masked_log_px_z <- log_px_z_unreduced * mask
     log_px_z <- tf$reduce_sum(masked_log_px_z, axis = -1L)
     
-    # Check other components
     log_qz_xu <- source_log_pdf(z_sample, z_mean, tf$math$exp(z_logvar))
     
     log_pz_u <- source_log_pdf(z_sample, prior_mean_final, tf$math$exp(prior_log_v))
@@ -253,47 +232,42 @@ iVAEar <- function(data, aux_data, latent_dim, prev_data_list, prev_aux_data_lis
     loss = vae_loss,
     metrics = list(metric_reconst_accuracy)
   )
-  validation_data <- NULL
-  if (!is.null(test_data_scaled)) {
-    validation_data <- list(list(test_data_scaled, test_data_prev, test_data_aux, test_data_aux), test_data_scaled)
-  }
+
   inputs <- list(data_scaled, aux_data, mask)
   inputs <- append(inputs, prev_data_list)  
   inputs <- append(inputs, prev_aux_data_list)
-  #if (!is.null(epoch_thresholds)) {
-  #  if (length(epoch_thresholds) != length(threshold_batch_sizes)) {
-  #    warning("If epoch_thresholds is set, threshold_batch_sizes must be a vector of equal length.")
-  #  } else {
-  #    
-  #  }
-  #}
-  hist <- vae %>% fit(inputs, data_scaled, validation_data = validation_data, 
+
+  hist <- vae %>% fit(inputs, data_scaled, 
     validation_split = validation_split, shuffle = TRUE, 
     batch_size = initial_batch_size, epochs = epochs)
   
   IC_estimates <- predict(encoder, list(data_scaled, aux_data))
-  obs_estimates <- predict(decoder, IC_estimates)
   IC_log_vars <- predict(z_log_var_model, list(data_scaled, 
       aux_data))
-  prior_means <- predict(prior_mean_model, aux_data)
-  if (ar_order == 1) {
-  prior_means_prev <- predict(prior_mean_model, prev_aux_data_list[[1]])
-  IC_estimates_prev <- predict(encoder,list(prev_data_list[[1]], prev_aux_data_list[[1]]))
-  prior_log_vars <- predict(prior_log_var_model, aux_data)
-  prior_ar1s <- predict(prior_ar_model, aux_data)
-  print("Calculating ELBO...")
-  prior_mean_ests <- prior_means + prior_ar1s * (IC_estimates_prev - prior_means_prev)
-  log_px_z <- error_log_pdf(tf$constant(data_scaled, "float32"), 
-      tf$cast(obs_estimates, "float32"), tf$constant(error_dist_sigma, 
-          "float32"))
-  log_qz_xu <- source_log_pdf(tf$cast(IC_estimates, "float32"), 
-      tf$cast(IC_estimates, "float32"), tf$math$exp(tf$cast(IC_log_vars, 
-          "float32")))
-  log_pz_u <- source_log_pdf(tf$cast(IC_estimates, "float32"), 
-      tf$cast(prior_mean_ests, "float32"), tf$math$exp(tf$cast(prior_log_vars, 
-          "float32")))
-  elbo <- tf$reduce_mean(log_px_z + log_pz_u - log_qz_xu, -1L)
+  if (get_elbo) {
+    print("Calculating ELBO...")
+    obs_estimates <- predict(decoder, IC_estimates)
+    prior_means <- predict(prior_mean_model, aux_data)
+    prior_mean_ests <- prior_means
+    for (i in 1:ar_order) {
+      prior_means_prev <- predict(prior_mean_model, prev_aux_data_list[[1]])
+      IC_estimates_prev <- predict(encoder,list(prev_data_list[[1]], prev_aux_data_list[[1]]))
+      prior_log_vars <- predict(prior_log_var_model, aux_data)
+      prior_ars <- predict(prior_ar_model, aux_data)
+      prior_mean_ests <- prior_mean_ests + prior_ars[, ((i - 1) * latent_dim + 1):(i * latent_dim)] * (IC_estimates_prev - prior_means_prev)
+    }
+    log_px_z <- error_log_pdf(tf$constant(data_scaled, "float32"), 
+        tf$cast(obs_estimates, "float32"), tf$constant(error_dist_sigma, 
+            "float32"))
+    log_qz_xu <- source_log_pdf(tf$cast(IC_estimates, "float32"), 
+        tf$cast(IC_estimates, "float32"), tf$math$exp(tf$cast(IC_log_vars, 
+            "float32")))
+    log_pz_u <- source_log_pdf(tf$cast(IC_estimates, "float32"), 
+        tf$cast(prior_mean_ests, "float32"), tf$math$exp(tf$cast(prior_log_vars, 
+            "float32")))
+    elbo <- tf$reduce_mean(log_px_z + log_pz_u - log_qz_xu, -1L)
   } else elbo <- NULL
+  
   IC_log_vars <- predict(z_log_var_model, list(data_scaled, aux_data))
   IC_means <- colMeans(IC_estimates)
   IC_sds <- apply(IC_estimates, 2, sd)
@@ -301,20 +275,9 @@ iVAEar <- function(data, aux_data, latent_dim, prev_data_list, prev_aux_data_lis
   IC_estimates_scaled <- sweep(IC_estimates_cent, 2, IC_sds, "/")
   IC_vars <- exp(IC_log_vars)
   IC_vars_scaled <- sweep(IC_vars, 2, IC_sds^2, "/")
-  prior_means_scaled <- NULL
-  prior_vars_scaled <- NULL
-  # if (get_prior_means) {
-  #   prior_ar1 <- predict(prior_ar_model, diag(dim_aux))
-  #   prior_log_vars <- predict(prior_log_var_model, diag(dim_aux))
-  #   prior_means_cent <- sweep(prior_means, 2, IC_means, "-")
-  #   prior_means_scaled <- sweep(prior_means_cent, 2, IC_sds, "/")
-  #   prior_vars <- exp(prior_log_vars)
-  #   prior_vars_scaled <- sweep(prior_vars, 2, IC_sds^2, "/")
-  # }
 
   iVAE_object <- list(
-    IC_unscaled = IC_estimates, IC = IC_estimates_scaled, IC_vars = IC_vars_scaled,
-    prior_means = prior_means_scaled, prior_vars = prior_vars_scaled, data_dim = p, latent_dim = latent_dim,
+    IC_unscaled = IC_estimates, IC = IC_estimates_scaled, data_dim = p, latent_dim = latent_dim,
     sample_size = n, prior_ar_model = prior_ar_model, call_params = call_params,
     aux_dim = dim_aux, encoder = encoder, decoder = decoder, data_means = data_means,
     data_sds = data_sds, IC_means = IC_means, IC_sds = IC_sds, call = deparse(sys.call()),
